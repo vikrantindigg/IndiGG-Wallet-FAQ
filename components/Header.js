0316bfc69ba6d456438ac8d88abf099db69280a4
vikrantindigg <121029127+vikrantindigg@users.noreply.github.com>
2023-01-06 22:36:20 +0530

    updated

--- components/Header.js
+++ components/Header.js
@@ -5,8 +5,8 @@ export default function Header({ name }) {
     <header className="pt-20 pb-12">
       <div className="w-12 h-12 rounded-full block mx-auto mb-4 bg-gradient-conic from-gradient-3 to-gradient-4" />
       <p className="text-2xl dark:text-white text-center">
-        <Link href="/">
-          <a>{name}</a>
+        <Link href="https://indi.gg/">
+          <a>IndiGG is India's biggest gaming guild with a mission to onboard 500m+ gamers from Indian subcontinent. We provide you with an opportunity to earn an income while playing video games daily, completely free of cost.</a>
         </Link>
       </p>
     </header>
