6282bc906920a1588cfc4e12ea3e23b57a867c76
vikrantindigg <121029127+vikrantindigg@users.noreply.github.com>
2023-01-06 22:09:51 +0530

    update

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Index({ posts, globalData }) {
     <Layout>
       <SEO title={globalData.name} description={globalData.blogTitle} />
       <Header name={globalData.name} />
-      <main className="w-full">
+      {/* <main className="w-full">
         <h1 className="text-3xl lg:text-5xl text-center mb-12">
           {globalData.blogTitle}
         </h1>
@@ -45,7 +45,7 @@ export default function Index({ posts, globalData }) {
             </li>
           ))}
         </ul>
-      </main>
+      </main> */}
       <Footer copyrightText={globalData.footerText} />
       <GradientBackground
         variant="large"
